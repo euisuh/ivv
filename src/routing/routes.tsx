01305a35bfc0c8a59e0e8b435e089cbc3ee56bc0
Euisuh (John) Jeong <euisuh.jeong@gmail.com>
2024-10-05 21:17:18 +0300

    hotifx

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,35 +1,16 @@
-import { Navigate, Outlet } from "react-router-dom";
-
 import Landing from "../features/landing/Landing";
 import Error from "./Error";
-import Gallery from "../features/gallery/Gallery";
 
 export const getRoutes = () => {
   return [
     {
       path: "/ivv",
-      element: <Outlet />,
+      element: <Landing />,
       errorElement: <Error />,
-      children: [
-        {
-          index: true,
-          element: <Navigate to="landing" replace />,
-        },
-        {
-          path: "landing",
-          element: <Landing />,
-          errorElement: <Error />,
-        },
-        {
-          path: "gallery",
-          element: <Gallery />,
-          errorElement: <Error />,
-        },
-        {
-          path: "*",
-          element: <Error />,
-        },
-      ],
+    },
+    {
+      path: "*",
+      element: <Error />,
     },
   ];
 };
