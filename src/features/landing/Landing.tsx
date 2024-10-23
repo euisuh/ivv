3f9afadd324051787a4c82d72914b9754bf7a5af
Euisuh (John) Jeong <euisuh.jeong@gmail.com>
2024-10-23 11:23:49 +0300

    add a little surprise gift for cutie iv

--- src/features/landing/Landing.tsx
+++ src/features/landing/Landing.tsx
@@ -1,5 +1,17 @@
 import { useState, useRef, useEffect } from "react";
-import { Alert, Box, Button, Snackbar, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Snackbar,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  TextField,
+} from "@mui/material";
 import { DateTime } from "luxon";
 
 import background1 from "../../assets/background/1.jpg";
@@ -22,6 +34,11 @@ function Landing() {
   const [randomBackground, setRandomBackground] = useState<string>("");
   const [numberAttempt, setNumberAttempt] = useState(0);
   const [timeRemaining, setTimeRemaining] = useState("");
+  const [timeRemainingBool, setTimeRemainingBool] = useState(true);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [answer, setAnswer] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+  const [correctAnswer, setCorrectAnswer] = useState(false);
 
   const contentRef = useRef<HTMLDivElement | null>(null);
   const noButtonRef = useRef<HTMLButtonElement | null>(null);
@@ -58,7 +75,6 @@ function Landing() {
     });
   }, []);
 
-  // Countdown timer logic
   useEffect(() => {
     const targetDate = DateTime.fromObject(
       { year: 2024, month: 10, day: 24, hour: 0, minute: 0, second: 0 },
@@ -85,12 +101,10 @@ function Landing() {
           timeRemaining += `${String(minutes).padStart(2, "0")}m `;
 
         timeRemaining += `${String(seconds).padStart(2, "0")}s`;
-
-        setTimeRemaining(timeRemaining.trim());
-
         setTimeRemaining(timeRemaining.trim());
       } else {
         setTimeRemaining("The time has arrived!");
+        setTimeRemainingBool(false);
       }
     };
 
@@ -144,6 +158,27 @@ function Landing() {
     }
   };
 
+  const handleLinkClick = () => {
+    setIsDialogOpen(true);
+    setCorrectAnswer(false);
+  };
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+    setShowMessage(false);
+    setCorrectAnswer(false);
+    setAnswer("");
+  };
+
+  const handleAnswerSubmit = () => {
+    if (answer.toLowerCase() === "guagua") {
+      setShowMessage(true);
+      setCorrectAnswer(true);
+    } else {
+      setShowMessage(true);
+    }
+  };
+
   return (
     <Box
       height="100vh"
@@ -185,11 +220,20 @@ function Landing() {
         <Typography variant="h2" sx={{ color: "black" }}>
           {text}
         </Typography>
-
-        <Typography variant="inherit" color="textSecondary" sx={{ mt: 2 }}>
-          {timeRemaining}
-        </Typography>
-
+        {timeRemainingBool ? (
+          <Typography variant="inherit" color="textSecondary" sx={{ mt: 2 }}>
+            {timeRemaining}
+          </Typography>
+        ) : (
+          <Button
+            variant="outlined"
+            size="small"
+            color="success"
+            onClick={handleLinkClick}
+          >
+            Yay
+          </Button>
+        )}
         <Box
           sx={{
             mt: 4,
@@ -229,6 +273,57 @@ function Landing() {
           </Button>
         </Box>
       </Box>
+
+      <Dialog
+        open={isDialogOpen}
+        fullWidth
+        maxWidth="md"
+        aria-modal="false"
+        onClose={handleDialogClose}
+      >
+        <DialogTitle sx={{ px: 4, pt: 4, mb: 2, pb: 0 }}>
+          Unlock the secret only Ivy can crack! 🔐🌟
+        </DialogTitle>
+        <DialogContent sx={{ px: 4, py: 0, mb: 2 }}>
+          {!showMessage ? (
+            <>
+              <DialogContentText>
+                Fill in the following content: Anthia ••• Ethan •••
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Your Answer"
+                fullWidth
+                variant="standard"
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+              />
+            </>
+          ) : correctAnswer ? (
+            <DialogContentText>
+              Ivy, I know you've been having a tough time, and I believe in you.
+              💖 Here's a little treat to keep you going—a Starbucks coffee on
+              me! ☕ Use this gift card to recharge and relax:{" "}
+              <strong>1234 5678 9012 3456</strong>. You've got this! 🌟
+            </DialogContentText>
+          ) : (
+            <DialogContentText>
+              This is not Ivy. Only Ivy can resolve this.
+            </DialogContentText>
+          )}
+        </DialogContent>
+        <DialogActions sx={{ pb: 4, px: 4, pt: 0 }}>
+          {!showMessage ? (
+            <>
+              <Button onClick={handleDialogClose}>Cancel</Button>
+              <Button onClick={handleAnswerSubmit}>Submit</Button>
+            </>
+          ) : (
+            <Button onClick={handleDialogClose}>Close</Button>
+          )}
+        </DialogActions>
+      </Dialog>
 
       <Snackbar
         open={openSnackbar}
